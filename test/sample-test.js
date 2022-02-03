850dccd5763c9966b95a93e34b1ce4b881c333aa
Abhijith <abhi@Abhijiths-MacBook-Air.local>
2022-02-04 00:06:56 +0530

    transferring tokens test

--- test/sample-test.js
+++ test/sample-test.js
@@ -17,10 +17,20 @@ describe("StakingRewards", function () {
   //   expect(await greeter.greet()).to.equal("Hola, mundo!");
   let StakingRewards;
   let stakingRewards;
+  let StakingToken;
+  let stakingToken;
+  let RewardsToken;
+  let rewardsToken;
+  let signers;
 
   beforeEach( async() => {
+    signers = await ethers.getSigners();
+
     StakingRewards = await ethers.getContractFactory("StakingRewards");
     stakingRewards = await StakingRewards.deploy("0x5FbDB2315678afecb367f032d93F642f64180aa3","0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
+
+    StakingToken = await ethers.getContractFactory("StakingToken");
+    stakingToken = await StakingToken.deploy();
 
   })
 
@@ -35,8 +45,20 @@ describe("StakingRewards", function () {
       const rewardTokenAddress = await stakingRewards.rewardsToken();
       expect(rewardTokenAddress).to.equal("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
     })
+    it("has the correct reward rate", async () => {
+      await stakingRewards.deployed();
+      const rewardRate = await stakingRewards.rewardRate();
+      expect(rewardRate).to.equal("100");
+    })
 
-
+  })
+  describe("Transferring Tokens", async () => {
+    it("can transfer 1000 STN tokens to user", async () => {
+      await stakingToken.deployed();
+      await stakingToken.transfer(signers[1].address, "1000");
+      const balance = await stakingToken.balanceOf(signers[1].address);
+      expect(balance).to.equal("1000");
+    })
   })
 
 });
